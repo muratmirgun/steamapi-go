4eeeebd41126c2fb3dd74ad868a2d83c0e888803
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-03-07 17:13:21 +0300

    get player summaries function json unmarshal method added

--- user.go
+++ user.go
@@ -1,6 +1,8 @@
-package steamapi
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,27 +13,49 @@ const (
 	MainPost string = "https://api.steampowered.com/"
 )
 
+type mainstruct struct {
+	Response ResponseStruct `json:"response"`
+}
+
+// ResponseStruct model
+type ResponseStruct struct {
+	Players []PlayersStruct `json:"players"`
+}
+
+// PlayersStruct model
+type PlayersStruct struct {
+	SteamID    string `json:"steamid"`
+	ProfileURL string `json:"profileurl"`
+}
+
 func errcheck(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-//GetPlayerSummaries: Input steamıd and collect user's all infos
-func GetPlayerSummaries(steamids string, PrivKey string) {
-	resp, err := http.Get(MainPost + "ISteamUser/GetPlayerSummaries/v2/?key=" + string(PrivKey) + "&steamids=" + steamids)
-	errcheck(err)
-	//We Read the response body on the line below.
+//GetPlayerSummaries: Input SteamID and collect user's all infos
+func GetPlayerSummaries(SteamID string, PrivyKey string) {
+	steamid := "76561197960361544"
+	resp, _ := http.Get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" + string(PrivKey) + "&steamids=" + string(steamid))
+
 	body, err := ioutil.ReadAll(resp.Body)
-	errcheck(err)
-	//Convert the body to type string
-	sb := string(body)
-	log.Printf(sb)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	textBytes := []byte(body)
+	main := mainstruct{}
+	err = json.Unmarshal(textBytes, &main)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(main.Response.Players)
 }
 
-//GetPlayerBans input user'ssteam id and get user's ban info
-func GetPlayerBans(steamids string, PrivKey string) {
-	resp, err := http.Get(MainPost + "ISteamUser/GetPlayerBans/v1/?key=" + string(PrivKey) + "&steamids=" + steamids)
+//GetPlayerBans input user's SteamID and get user's ban info
+func GetPlayerBans(SteamID string, PrivyKey string) {
+	resp, err := http.Get(MainPost + "ISteamUser/GetPlayerBans/v1/?key=" + string(PrivyKey) + "&steamids=" + SteamID)
 	errcheck(err)
 	//We Read the response body on the line below.
 	body, err := ioutil.ReadAll(resp.Body)
